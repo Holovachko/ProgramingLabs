5297d9d9440c6b6c589ba1dcff92675adf103722
Holovachko <74302993+Holovachko@users.noreply.github.com>
2021-09-25 17:08:32 +0300

    Add files via upload

---
+++ Lab1XML/Pretendent.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Xml.Serialization;
+
+namespace Lab1XML
+{
+    [Serializable]
+    public class Pretendent
+    {
+        public string Fullname { get; set; }
+        public string EyesColor { get; set; }
+        public double Salary { get; set; }
+        public int Age { get; set; }
+        public bool Apartament  { get; set; }
+
+        public Pretendent(string fullname , string eyescolor , double salary , int age , bool apartament)
+        {
+            Fullname = fullname;
+            EyesColor = eyescolor;
+            Salary = salary;
+            Age = age;
+            Apartament = apartament;
+        }
+        public Pretendent()
+        {
+
+        }
+    }
+    
+}
---
+++ Lab1XML/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml.Serialization;
+using System.IO;
+namespace Lab1XML
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Pretendent person1 = new Pretendent("Anton Ivanov" , "green" , 10000 , 18 , true);
+            Pretendent person2 = new Pretendent("Ivan Petrenko" , "blue" , 5000 , 21 , false);
+            Pretendent person3 = new Pretendent("Misha Zaharov" ,"blue" , 20000 , 30 , true);
+            Pretendent person4 = new Pretendent("Vasyl Kiselev" , "green" , 50000 , 33 , true);
+            Pretendent person5 = new Pretendent("John Shelby" , "brown" , 15000 , 23 , false);
+            Pretendent[] pretends = new Pretendent[] { person1, person2, person3, person4, person5 };
+            XmlSerializer formatter = new XmlSerializer(typeof(Pretendent[]));
+
+            using (FileStream fs = new FileStream("staff.xml", FileMode.OpenOrCreate))
+            {
+                formatter.Serialize(fs, pretends);
+            }
+
+            using (FileStream fs = new FileStream("staff.xml", FileMode.OpenOrCreate))
+            {
+                Pretendent[] newEmployee = (Pretendent[])formatter.Deserialize(fs);
+
+                foreach (Pretendent e in newEmployee)
+                {
+                    Console.WriteLine($" {e.Fullname}  {e.Apartament}  {e.Salary}  {e.Age}  {e.EyesColor}");
+                }
+            }
+            Console.ReadLine();
+        }
+    }
+    }
+
